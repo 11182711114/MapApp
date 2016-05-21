9502c4ef6bc2e88c702068b5a27aafd23b4a6690
Fredrik Larsson <flarsson93@gmail.com>
2016-05-21 03:17:11 +0200

    comment

--- src/places/Category.java
+++ src/places/Category.java
@@ -8,8 +8,7 @@ import java.util.Set;
 
 public class Category {
 //	private static HashMap<String,Category> currentCategories;
-	
-	private static class CategoryHolder{
+	private static class CategoryHolder{ //Does this even make sense?
 		private static final Map<String,Category> CURRENT_CATEGORIES = new HashMap<>();
 		static{
 			CURRENT_CATEGORIES.put("Buss", new Category("Buss",Color.red));	
